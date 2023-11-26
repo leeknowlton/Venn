0fe3317d6198c2334049fa3fb581ffe5368103fc
Lee Knowlton <leeknowlton@gmail.com>
2023-11-26 23:37:12 +0900

    Add working streaming

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,16 @@
 <script>
+	import { onMount } from 'svelte';
 	import { marked } from 'marked';
 	import DOMPurify from 'dompurify';
 
 	let concept = 'Random Forests';
-	let renderedExplanation = '';
+	let explanation = '';
+	let answer;
 
-	async function explain() {
+	let result = '';
+
+	async function getStream() {
+		result = '';
 		const response = await fetch('/api/openai', {
 			method: 'POST',
 			body: JSON.stringify({ concept }),
@@ -13,20 +18,41 @@
 				'content-type': 'application/json'
 			}
 		});
+		const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
+		let accumulated = ''; // Accumulate chunks here
 
-		const explanation = await response.json();
-		console.log(explanation);
-		const unsafeExplanation = marked(explanation);
-		renderedExplanation = DOMPurify.sanitize(unsafeExplanation);
+		while (true) {
+			const { value, done } = await reader.read();
+			if (done) break;
+
+			accumulated += value;
+
+			// Split accumulated data by newline
+			let newlineIndex;
+			while ((newlineIndex = accumulated.indexOf('\n')) !== -1) {
+				const jsonString = accumulated.substring(0, newlineIndex);
+				accumulated = accumulated.substring(newlineIndex + 1);
+
+				try {
+					const json = JSON.parse(jsonString);
+					if (json.choices && json.choices[0] && json.choices[0].delta) {
+						result += json.choices[0].delta.content;
+					}
+				} catch (error) {
+					console.error('Error parsing chunk:', error);
+				}
+			}
+		}
 	}
 </script>
 
 <div class="w-1/2 m-auto my-4">
+	<h1 class="text-2xl my-6">ELI5</h1>
 	<div class="flex gap-5">
 		<input class="input input-primary" bind:value={concept} />
-		<button class="btn btn-primary" type="button" on:click={explain}> Explain </button>
+		<button class="btn btn-primary" type="button" on:click={getStream}> Explain </button>
 	</div>
-	<div class="my-4 prose">{@html renderedExplanation}</div>
+	<div class="my-4 prose">{result}</div>
 
 	<br />
 </div>
--- src/routes/api/openai/+server.js
+++ src/routes/api/openai/+server.js
@@ -1,27 +1,30 @@
 import OpenAI from 'openai';
-
-/** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
 	const data = await request.json();
 	console.log(data);
 
 	let concept = data.concept;
-
+	// const concept = url.searchParams.get('concept');
+	// let concept = 'Random Forests';
 	const openai = new OpenAI({ apiKey: import.meta.env.VITE_OPENAI_API_KEY });
 
-	const completion = await openai.chat.completions.create({
+	const stream = await openai.chat.completions.create({
 		messages: [
 			{
 				role: 'system',
 				content:
-					'You are a helpful teacher that breaks concepts down into extremely simple langauge (ELI5). Format your answer in markdown'
+					'You are a helpful teacher that breaks concepts down into extremely simple language (ELI5).'
 			},
 			{ role: 'user', content: `Explain ${concept}` }
 		],
-		model: 'gpt-3.5-turbo'
+		model: 'gpt-3.5-turbo',
+		stream: true
 	});
 
-	console.log(completion.choices[0]);
-
-	return new Response(JSON.stringify(completion.choices[0].message.content));
+	return new Response(stream.toReadableStream(), {
+		status: 200,
+		headers: {
+			'Content-Type': 'text/event-stream'
+		}
+	});
 }
